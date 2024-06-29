4f02c5769e78bcea6fc7de2e6a45aa902028569b
Shamira Umstead <sumstead@usf.edu>
2024-06-28 23:55:24 -0400

    git commit -m "Develop investment return calculation module."

--- investmentreturn.js
+++ investmentreturn.js
@@ -1,5 +1,4 @@
 // U78784426
 // modules/investmentReturn.js
 export function calculateInvestmentReturn(principal, rate, timesCompounded, years) {
-    return principal * Math.pow((1 + rate / timesCompounded), timesCompounded * years);
-}
+    return principal * Math.pow((1 + rate / timesCompounded), timesCompounded * years);}
